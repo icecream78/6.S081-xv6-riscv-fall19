2235ba67636322d089fc77d3a01dddcfaa8e1b1c
Faker <mr.bes1996@mail.ru>
2020-06-10 23:45:36 +0300

    proper closing unused pipes

--- user/pingpong.c
+++ user/pingpong.c
@@ -18,16 +18,21 @@ int main(int argc, char *argv[]) {
         write(fdsIn[1], pingText, strlen(pingText));
         wait();
 
+        close(fdsIn[1]);
+
         read(fdsOut[0], bufOut, sizeof(bufOut));
+        close(fdsOut[0]);
         int ppid = getpid();
         printf("%d: received %s\n", ppid, bufOut);
     } else if (pid == 0) {
         read(fdsIn[0], bufIn, sizeof(bufIn));
+        close(fdsIn[0]);
 
         int ppid = getpid();
         printf("%d: received %s\n", ppid, bufIn);
 
         write(fdsOut[1], pongText, strlen(pongText));
+        close(fdsOut[1]);
         exit();
     } else {
     }
